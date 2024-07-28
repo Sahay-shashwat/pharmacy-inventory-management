4e1c68b12e65bc576bfc1234c85ebec1ba4155ef
khapekar-sayali <sayalikhapekar2805@gmail.com>
2024-07-28 19:12:04 +0530

    endpoint for inventory

--- main.py
+++ main.py
@@ -136,6 +136,10 @@ def purchase_register():
     
     return jsonify({"message": "Data received successfully"})
 
+@app.route("/inventory-repo")
+def vendor():
+    return render_template("inventory.html") 
+
 @app.route('/get_items', methods=['GET'])
 def get_items():
     items=db.getColumn("Product_name","item_master")
